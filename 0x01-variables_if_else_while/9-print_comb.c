668984e3f5a4d07d78368462de7534b177b87bda
Momen Sameh <momensameh5@gmail.com>
2023-07-17 17:44:26 -0700

    added 9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int n = 48;
+
+	while (n < 58)
+	{
+		if (n != 57)
+		{
+			putchar(n);
+			putchar(44);
+			putchar(32);
+		}
+		else
+		{
+			putchar(n);
+		}
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
