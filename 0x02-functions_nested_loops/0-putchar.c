70096ec847eb0ac3fb8c0556c855282a0c070296
Momen Sameh <momensameh5@gmail.com>
2023-07-19 16:15:49 -0700

    added 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* main - function prints "_putchar" to stdout
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char s[10] = "_putchar";
+
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(school[i]);
+	}
+	_putchar(10);
+	return (0);
+}
