d32ce9eb4d9e47abc4496e64083244e684c0a48d
Momen Sameh <momensameh5@gmail.com>
2023-07-19 16:39:37 -0700

    added 2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* print_alphabet_x10 - prints the alphabet in lowercase 10 times
+*
+* Return: void
+*/
+void print_alphabet_x10(void)
+{
+	int i;
+	int j;
+	
+	for (j = 0; j < 10; j++){
+		for (i = 97; i < 123; i++)
+		{
+			_putchar(i);
+		}
+	}
+
+	_putchar(10);
+}
