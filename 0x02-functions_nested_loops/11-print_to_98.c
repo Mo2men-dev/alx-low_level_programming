61f936343271d6ee327295101ca437bcaef283e7
Momen Sameh <momensameh5@gmail.com>
2023-07-19 19:09:07 -0700

    added print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,24 @@
+/**
+* print_to_98 - prints numbers from n to 98
+* @n: number to start from
+* Return: returns nothing
+*/
+void print_to_98(int n)
+{
+	if (n >= 98)
+	{
+		while (n > 98)
+		{
+			printf("%d, ", n--);
+		}
+		printf("%d\n", n);
+	}
+	else
+	{
+		while (n < 98)
+		{
+			printf("%d, ", n++);
+		}
+		printf("%d\n", n);
+	}
+}
