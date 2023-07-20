82c65d60a7ed06c97007a62ac7ab4a7ee7314577
Momen Sameh <momensameh5@gmail.com>
2023-07-19 17:15:12 -0700

    added 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* _abs - returns the abs value of a num
+* @n: num to be checked
+* Return: a positve num or 0
+*/
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+	if (n < 0)
+	{
+		return (-num);
+	}
+	_putchar(48);
+	return (0);
+}
