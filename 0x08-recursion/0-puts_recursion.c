47195501da7ab42e6e68326ce3bd0250d5d828e1
Momen Sameh <momensameh5@gmail.com>
2023-08-02 17:32:04 -0700

    edited 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,19 +3,21 @@
  * @s: Pointer to the string to be printed.
  *
  * Description: This function prints the characters of a string recursively
- *              until it reaches the null terminator '\0', then prints a newline.
+ * until it reaches the null terminator '\0', then prints a newline.
+ *
+ * Return: Nothing (void)
  */
 void _puts_recursion(char *s)
 {
 	if (*s != '\0')
 	{
-		putchar(*s); /* Print the current character */
-		s++; /* Move to the next character in the string */
-		_puts_recursion(s); /* Recursively call the function with the updated pointer */
+		_putchar(*s);
+		s++;
+		_puts_recursion(s);
 	}
 	else
 	{
-		putchar(10); /* Print a newline when the null terminator is reached */
+		_putchar(10); /* Print a newline when the null terminator is reached */
 	}
 }
 
