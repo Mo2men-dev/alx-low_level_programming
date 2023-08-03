f2d30dc6290462af5c7dd4ba7148f91b777af33f
Momen Sameh <momensameh5@gmail.com>
2023-08-02 17:57:32 -0700

    added 1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+/**
+ * _print_rev_recursion - Prints a string in reverse using recursion.
+ * @s: Pointer to the string to be printed in reverse.
+ *
+ * Description: This function prints the characters of a string recursively
+ *              in reverse order.
+ * Return: Nothing (0)
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		putchar(*s);
+	}
+}
+
