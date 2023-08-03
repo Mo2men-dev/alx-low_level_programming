990ce3ed057f8d35596adffe205f10cef93f429e
Momen Sameh <momensameh5@gmail.com>
2023-08-02 20:34:49 -0700

    added 5-sqrt_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * find_sqrt - Helper function to find the square root using binary search.
+ * @n: The number for which to find the square root.
+ * @start: The starting index for binary search.
+ * @end: The ending index for binary search.
+ *
+ * Return: The square root of n if found, -1 if not found.
+ */
+int find_sqrt(int n, int start, int end)
+{
+	if (start > end)
+		return (-1);
+
+	int mid = start + (end - start) / 2;
+	long long square = (long long)mid * mid;
+
+	if (square == n)
+		return (mid);
+	if (square < n)
+		return (find_sqrt(n, mid + 1, end));
+	return (find_sqrt(n, start, mid - 1));
+}
+
+/**
+ * _sqrt_recursion - Returns the natural square root of a number.
+ * @n: The number for which to find the square root.
+ *
+ * Return: The square root of n if found, -1 if not found.
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+
+	return (find_sqrt(n, 0, n));
+}
+
+
