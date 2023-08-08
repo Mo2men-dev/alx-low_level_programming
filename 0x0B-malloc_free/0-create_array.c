27fbd35a4ecd6c8aa033318179644e23ac16d72a
Momen Sameh <momensameh5@gmail.com>
2023-08-08 16:33:58 -0700

    edited 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -7,32 +7,34 @@
  * @c: The character value to initialize the array with.
  *
  * Return: If size is 0 or if memory allocation fails, returns a pointer
- *         to a null-terminated string containing just '\0'. Otherwise,
- *         returns a pointer to the newly created array.
+ *         to a null-terminated string containing just '\0'.
+ *         Otherwise, returns a pointer to the newly created array.
  */
 char *create_array(unsigned int size, char c)
 {
-        char nl = '\0';
-        char *ptr = &nl;
+	char nl = '\0';
+	char *ptr = &nl;
+	char *s;
+	unsigned int i;
 
-        if (size == 0)
-        {
-                return (ptr);
-        }
+	if (size == 0)
+	{
+		return (ptr);
+	}
 
-        char *s = malloc(size * sizeof(c));
+	s = malloc(size * sizeof(c));
 
-        if (s == '\0')
-        {
-                return (ptr);
-        }
+	if (s == NULL)
+	{
+		return (ptr);
+	}
 
-        unsigned int i = 0;
-        while (i < size)
-        {
-                s[i] = c;
-                i++;
-        }
+	i = 0;
+	while (i < size)
+	{
+		s[i] = c;
+		i++;
+	}
 
-        return (s);
+	return (s);
 }
