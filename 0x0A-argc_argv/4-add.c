5301a6a9ff7eaba46a80d7f5ef6b9e5e6f2a3f59
Momen Sameh <momensameh5@gmail.com>
2023-08-09 18:36:47 -0700

    added 4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+/**
+ * isInteger - Check if a string is a valid integer.
+ * @str: The input string to check.
+ *
+ * Return: true if the string is a valid integer, false otherwise.
+ */
+bool isInteger(const char *str)
+{
+	if (*str == '\0')
+	{
+		return (false);
+	}
+	while (*str != '\0')
+	{
+		if (*str < '0' || *str > '9')
+		{
+			return (false);
+		}
+		str++;
+	}
+	return (true);
+}
+
+/**
+ * main - Entry point of the program.
+ * @argc: Number of command-line arguments.
+ * @argv: Array of strings containing the arguments.
+ *
+ * Return: 0 on success, 1 on error.
+ */
+int main(int argc, char *argv[])
+{
+	int sum = 0;
+
+	for (int i = 1; i < argc; i++)
+	{
+		if (!isInteger(argv[i]))
+		{
+			printf("Error\n");
+			return (1);
+		}
+		sum += atoi(argv[i]);
+	}
+
+	printf("%d\n", sum);
+	return (0);
+}
+
