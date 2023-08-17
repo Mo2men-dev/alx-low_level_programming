46bca111961e6791721cdb158927c8b9a0e0062c
Momen Sameh <momensameh5@gmail.com>
2023-08-16 19:21:19 -0700

    added main.c op_functions.c get_op_func.c calc.h

---
+++ 0x0F-function_pointers/op_functions.c
@@ -0,0 +1,52 @@
+#include "3-calc.h"
+
+/**
+ * op_add - Function that sums up two numbers
+ * @a: first int to add
+ * @b: secound int to add
+ * Return: integer
+*/
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - Function that subtracts two numbers
+ * @a: first int
+ * @b: secound int
+ * Return: integer
+*/
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - Function that multipluies
+ * @a: first int
+ * @b: secound int
+ * Return: integer
+*/
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - Function that division
+ * @a: first int
+ * @b: secound int
+ * Return: integer
+*/
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - Function that module
+ * @a: first int
+ * @b: secound int
+ * Return: integer
+*/
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
