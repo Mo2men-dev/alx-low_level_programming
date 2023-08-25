582e0f404a9b78178639c1d30bd1c7f4fd645b9a
Momen Sameh <momensameh5@gmail.com>
2023-08-25 15:04:14 -0700

    edited 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -6,12 +6,14 @@
  *
  * Return: The length of the string.
  */
-int _strlen(const char *str) {
-	if (*str == '\0') {
-		return 0;
+int _strlen(const char *str)
+{
+	if (*str == '\0')
+	{
+		return (0);
 	}
 
-	return 1 + _strlen(str + 1);
+	return (1 + _strlen(str + 1));
 }
 
 /**
@@ -21,17 +23,20 @@ int _strlen(const char *str) {
  *
  * Return: Pointer to the new node.
  */
-list_t *add_node(list_t **head, const char *str) {
+list_t *add_node(list_t **head, const char *str)
+{
 	list_t *new_el = malloc(sizeof(list_t));
 
-	if (new_el == NULL) {
-		return NULL;
+	if (new_el == NULL)
+	{
+		return (NULL);
 	}
 
 	new_el->str = strdup(str);
-	if (new_el->str == NULL) {
+	if (new_el->str == NULL)
+	{
 		free(new_el);
-		return NULL;
+		return (NULL);
 	}
 
 	new_el->len = _strlen(str);
@@ -39,6 +44,6 @@ list_t *add_node(list_t **head, const char *str) {
 
 	*head = new_el;
 
-	return new_el;
+	return (new_el);
 }
 
