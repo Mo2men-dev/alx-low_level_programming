d0cd975d374120b314830171f443141232c0ff8c
Momen Sameh <momensameh5@gmail.com>
2023-09-08 17:32:08 -0700

    added 2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -15,28 +15,30 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd, bytes_written;
+	int fd;
+	int nletters;
+	int rwr;
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
-
-	if (text_content == NULL)
-		return (1);
 
 	fd = open(filename, O_WRONLY | O_APPEND);
 
 	if (fd == -1)
 		return (-1);
 
-	bytes_written = write(fd, text_content, 0);
-
-	if (bytes_written == -1)
+	if (text_content)
 	{
-		close(fd);
-		return (-1);
+		for (nletters = 0; text_content[nletters]; nletters++)
+			;
+
+		rwr = write(fd, text_content, nletters);
+
+		if (rwr == -1)
+			return (-1);
 	}
 
 	close(fd);
+
 	return (1);
 }
-
