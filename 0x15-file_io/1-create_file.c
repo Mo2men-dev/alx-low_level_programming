613952db9c0e3b6d67c10e94ad099bc97559b73d
Momen Sameh <momensameh5@gmail.com>
2023-09-08 17:21:51 -0700

    added 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,33 +16,29 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
-	ssize_t bytes_written;
-	size_t len = 0;
+	int nletters;
+	int rwr;
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
 
-	while (filename[len] != '\0')
-	{
-		len++;
-	}
-
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (fd == -1)
 		return (-1);
 
-	if (text_content != NULL)
-	{
-		bytes_written = write(fd, text_content, len);
-		if (bytes_written == -1)
-		{
-			close(fd);
-			return (-1);
-		}
-	}
+	if (!text_content)
+		text_content = "";
+
+	for (nletters = 0; text_content[nletters]; nletters++)
+		;
+
+	rwr = write(fd, text_content, nletters);
+
+	if (rwr == -1)
+		return (-1);
 
 	close(fd);
+
 	return (1);
 }
-
