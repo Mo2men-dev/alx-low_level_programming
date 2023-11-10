02c05daa0660965c91139cf088fb119a4f791f6a
Momen Sameh <momensameh5@gmail.com>
2023-11-10 14:28:41 -0800

    edited 7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -7,33 +7,34 @@
  * @idx: Position at the insert the new node
  * Return: The direction of the new node
  */
-
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	unsigned int c = 0;
-	dlistint_t *temp = *h;
-	dlistint_t *new = malloc(sizeof(dlistint_t));
-
-	if (new == NULL)
-		return (NULL);
+	dlistint_t *new_mem, *res_mem = *h;
+	/* unsigned int counter = 0 */
 
 	if (idx == 0)
 		return (add_dnodeint(h, n));
 
-	while (idx != c && temp != NULL)
+	for (; idx != 1; idx--)
 	{
-		c++;
-		temp = temp->next;
+		if (res_mem == NULL)
+			return (NULL);
+		res_mem = res_mem->next;
 	}
 
-	if (c < idx)
+	if (res_mem->next == NULL)
 		return (add_dnodeint_end(h, n));
 
-	new->n = n;
-	new->next = temp->next;
-	new->prev = temp;
-	temp->next->prev = new;
-	temp->next = new;
+	new_mem = malloc(sizeof(dlistint_t));
 
-	return (new);
+	if (new_mem == NULL)
+		return (NULL);
+
+	new_mem->n = n;
+	new_mem->next = res_mem->next;
+	new_mem->prev = res_mem;
+	res_mem->next->prev = new_mem;
+	res_mem->next = new_mem;
+
+	return (new_mem);
 }
