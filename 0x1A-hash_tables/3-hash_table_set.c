97fb69b90f1a0a3acf86b817e8444e199f71fb12
Momen Sameh <momensameh5@gmail.com>
2023-12-05 19:46:08 -0800

    edited 3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -65,7 +65,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (ht == NULL || key == NULL)
 		return (0);
-	index = hash_djb2((unsigned char *)key) % ht->size;
+	index = key_index((unsigned char *)key, ht->size);
 	node = ht->array[index];
 	if (node == NULL)
 	{
