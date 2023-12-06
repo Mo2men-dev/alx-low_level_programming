b7788b3df08cdd54f462de36e19572d0c7dddfe6
Momen Sameh <momensameh5@gmail.com>
2023-12-05 17:29:07 -0800

    edited 0-hash_table_create.c

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -18,11 +18,12 @@ hash_table_t *hash_table_create(unsigned long int size)
 	}
 
 	table->size = size;
-	table->array = malloc(sizeof((hash_table_t *) *size));
+	table->array = malloc(sizeof(hash_table_t *) * size);
 
 	if (table->array == NULL)
 	{
 		fprintf(stderr, "Error: malloc failed\n");
+		free(table);
 		return (NULL);
 	}
 
