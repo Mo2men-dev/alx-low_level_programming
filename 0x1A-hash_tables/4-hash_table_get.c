8cf2828e92a9157ba908ba044b8f43138584e50f
Momen Sameh <momensameh5@gmail.com>
2023-12-06 15:13:48 -0800

    edited 4-hash_table_get.c

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -12,7 +12,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	hash_node_t *node;
 	unsigned long int index = key_index((const unsigned char *)key, ht->size);
 
-	if (ht == NULL || key == NULL)
+	if (ht == NULL || key == NULL || strcmp(key, "") == 0)
 		return (NULL);
 
 	node = ht->array[index];
