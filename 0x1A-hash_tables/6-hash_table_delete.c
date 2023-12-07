6d0e7f46172feccb3acc88a94ff25c1726841ae3
Momen Sameh <momensameh5@gmail.com>
2023-12-06 16:58:20 -0800

    edited 6-hash_table_delete.c

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -9,22 +9,19 @@
 void hash_table_delete(hash_table_t *ht)
 {
 	unsigned long int i;
-	hash_node_t *node;
-
-	if (ht == NULL)
-		return;
+	hash_node_t *current;
 
 	for (i = 0; i < ht->size; i++)
 	{
 		if (ht->array[i] != NULL)
 		{
-			while (node != NULL)
+			while (ht->array[i] != NULL)
 			{
-				node = ht->array[i]->next;
-				free(node->key);
-				free(node->value);
-				free(node);
-				ht->array[i] = node;
+				current = ht->array[i]->next;
+				free(ht->array[i]->key);
+				free(ht->array[i]->value);
+				free(ht->array[i]);
+				ht->array[i] = current;
 			}
 		}
 	}
