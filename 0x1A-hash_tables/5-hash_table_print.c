23919c6513a57ad5f0a3c0ec093762bb51506a43
Momen Sameh <momensameh5@gmail.com>
2023-12-06 16:30:23 -0800

    added 5-hash_table_print.c

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,36 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - Print whole the hash table.
+ * @ht: Hash table.
+ *
+ * Return: Void.
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i, flag = 0;
+	hash_node_t *node;
+
+	if (ht == NULL)
+		return;
+
+	printf("{");
+
+	for (i = 0; i < ht->size; i++)
+	{
+		node = ht->array[i];
+		if (node != NULL)
+		{
+			while (node != NULL)
+			{
+				if (flag == 1)
+					printf(", ");
+				printf("'%s': '%s'", node->key, node->value);
+				flag = 1;
+				node = node->next;
+			}
+		}
+	}
+	printf('}');
+	printf('\n');
+}
